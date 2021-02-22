0f906279a3b5d3fabd6c78a5d7cd7cfda9eb4a52
milovanov <milovanov@myservices.digital>
2021-02-22 20:29:32 +0300

    Изменил число потоков и размер просматриваемой карты

--- app/src/main/java/ru/bigint/Main.java
+++ app/src/main/java/ru/bigint/Main.java
@@ -21,10 +21,10 @@ public class Main {
     private final static String schema = "http";
     private final static String URI = schema + "://" + address + ":" + port;
 
-    private final static int areaSize = 350;
+    private final static int areaSize = 500;
     private final static int maxDepth = 10;
 
-    private final static int threadsCount = 2;
+    private final static int threadsCount = 20;
 
 
     public static void main(String[] args) throws IOException, InterruptedException {
