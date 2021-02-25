6c3c2d5646f0f3fd0a5ed2c2f6d65805402d0a03
Mirovan <mirovan@narod.ru>
2021-02-25 19:00:00 +0300

    Исследование ошибки с превышением глубины

--- app/src/main/java/ru/bigint/Main.java
+++ app/src/main/java/ru/bigint/Main.java
@@ -49,6 +49,8 @@ public class Main {
                     //Пока есть сокровища и глубина позволяет - копать
                     int currentTreasureCount = pointTreasureCount;
                     while (currentTreasureCount > 0 && point.getDepth() < maxDepth) {
+                        if (point.getDepth() >= 11) System.out.println("->>>>>>>>>>> ERROR - too much depth ");
+
                         //Проверка - если нет лицензий - то надо получить лицензии многопоточно
                         if (client.getLicenses() == null || client.getLicenses().size() == 0) {
 
