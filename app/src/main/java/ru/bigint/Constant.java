f4a60fabe67e0afd4c455b237ea0a66f6f3250b2
Mirovan <mirovan@narod.ru>
2021-03-16 19:18:02 +0300

    update coords

--- app/src/main/java/ru/bigint/Constant.java
+++ app/src/main/java/ru/bigint/Constant.java
@@ -1,7 +1,7 @@
 package ru.bigint;
 
 public class Constant {
-    public final static String version = "hardcode_solve_x_208";
+    public final static String version = "hardcode_solve_x_237";
 
     //Общий размер карты [0..3499]
     public final static int mapSize = 3499;
