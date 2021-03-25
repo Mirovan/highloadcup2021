817e41d518bf573b62110ab71801555619837659
Mirovan <mirovan@narod.ru>
2021-03-25 15:56:16 +0300

    Добавил bat файл со сборкой jar и докера

--- app/build.bat
+++ app/build.bat
@@ -1,5 +1,6 @@
-docker build -t mirovan/hl2021 .
-docker tag mirovan/hl2021 stor.highloadcup.ru/rally/hamster_flyer
-docker push stor.highloadcup.ru/rally/hamster_flyer
+call mvn clean package
+call docker build -t mirovan/hl2021 .
+call docker tag mirovan/hl2021 stor.highloadcup.ru/rally/hamster_flyer
+call docker push stor.highloadcup.ru/rally/hamster_flyer
 
-#docker run -it -e ADDRESS=192.168.1.176 mirovan/hl2021
+@REM docker run -it -e ADDRESS=192.168.1.176 mirovan/hl2021
